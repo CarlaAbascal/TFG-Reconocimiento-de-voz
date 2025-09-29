a3c7e4d693dddf3f522056d8436b9bacabc60618
CarlaAbascal <carla.abascal@estudiantat.upc.edu>
2025-09-29 11:09:08 +0200

    version 1.3

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -35,7 +35,6 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // altLbl
@@ -116,23 +115,11 @@
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
-            // button7
-            // 
-            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(237, 233);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(178, 50);
-            this.button7.TabIndex = 8;
-            this.button7.Text = "Detener";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(427, 450);
-            this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -155,7 +142,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Speech.Recognition;
 using System.Windows.Forms;
 using csDronLink;
-using System.Speech.Recognition; // Reconocimiento de voz
 
 namespace WindowsFormsApp1
 {
@@ -20,15 +16,23 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
-            CheckForIllegalCrossThreadCalls = false; // Para evitar problemas de hilos con los labels
+            CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += Form1_FormClosing;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // INICIALIZAMOS RECONOCIMIENTO DE VOZ
-            recognizer = new SpeechRecognitionEngine();
+            // Intentar usar recognizer en español (fallback a constructor por defecto si falla)
+            try
+            {
+                recognizer = new SpeechRecognitionEngine(new CultureInfo("es-ES"));
+            }
+            catch
+            {
+                recognizer = new SpeechRecognitionEngine();
+            }
 
-            // Definimos comandos de voz
+            // Definimos comandos de voz (añade variantes si sueles decir frases)
             Choices commands = new Choices();
             commands.Add(new string[] {
                 "conectar",
@@ -36,8 +40,9 @@ namespace WindowsFormsApp1
                 "aterrizar",
                 "derecha",
                 "izquierda",
-                "avanzar",
-                "detener"
+                "girar a la derecha",
+                "girar a la izquierda",
+                "avanzar"
             });
 
             GrammarBuilder gb = new GrammarBuilder();
@@ -46,8 +51,30 @@ namespace WindowsFormsApp1
             Grammar g = new Grammar(gb);
             recognizer.LoadGrammar(g);
 
-            // Cuando reconoce algo
+            // Eventos principales
             recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+
+            // Reintentos automáticos si se completa o se rechaza
+            recognizer.RecognizeCompleted += (s, ev) =>
+            {
+                // si hubo error; volvemos a escuchar siempre que no haya sido disposed
+                try
+                {
+                    if (recognizer != null)
+                        recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                }
+                catch { }
+            };
+
+            recognizer.SpeechRecognitionRejected += (s, ev) =>
+            {
+                try
+                {
+                    if (recognizer != null)
+                        recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                }
+                catch { }
+            };
 
             recognizer.SetInputToDefaultAudioDevice();
             recognizer.RecognizeAsync(RecognizeMode.Multiple);
@@ -56,14 +83,17 @@ namespace WindowsFormsApp1
         private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             string comando = e.Result.Text;
+            float conf = (float)e.Result.Confidence;
 
-            this.BeginInvoke((MethodInvoker)delegate //Mete la acción en cola de la UI y no bloquea el reconocimiento
+          
+            // Ejecutar en cola de UI (no bloquea el hilo del recognizer)
+            this.BeginInvoke((MethodInvoker)delegate
             {
-                this.Text = $"Reconocido: {comando}";
+                // feedback
+                this.Text = $"Reconocido: {comando} (conf {conf:F2})";
 
                 switch (comando)
                 {
-                    //Simula el click a los botones 
                     case "conectar":
                         button1.PerformClick();
                         break;
@@ -74,23 +104,21 @@ namespace WindowsFormsApp1
                         button3.PerformClick();
                         break;
                     case "derecha":
+                    case "girar a la derecha":
                         button4.PerformClick();
                         break;
                     case "izquierda":
+                    case "girar a la izquierda":
                         button5.PerformClick();
                         break;
                     case "avanzar":
                         button6.PerformClick();
                         break;
-                    case "detener":
-                        button7.PerformClick();
-                        break;
                 }
             });
         }
 
-
-        // Recorrer la lista con los datos de telemetría y mostrar la altitud en un label
+        // Telemetría (sin cambios)
         private void ProcesarTelemetria(byte id, List<(string nombre, float valor)> telemetria)
         {
             foreach (var t in telemetria)
@@ -118,37 +146,49 @@ namespace WindowsFormsApp1
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // ya usabas bloquear:false, correcto para que no bloquee la UI
             miDron.Despegar(20, bloquear: false, f: EnAire, param: "Volando");
             button2.BackColor = Color.Yellow;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            miDron.Aterrizar();
+            // usar no bloqueante
+            miDron.Aterrizar(bloquear: false);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            // Girar a la derecha 90º
-            miDron.CambiarHeading(90);
+            // Girar a la derecha a 90º, no bloqueante
+            miDron.CambiarHeading(90, bloquear: false);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            // Girar a la izquierda -90º
-            miDron.CambiarHeading(-90);
+            // Girar a la izquierda: el -90 NO funciona -- por lo tanto 270
+            miDron.CambiarHeading(270, bloquear: false);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            // Avanzar 10 m
-            miDron.Mover("Forward", 10);
+            // Avanzar 10 m, modo no bloqueante
+            miDron.Mover("Forward", 10, bloquear: false);
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            // Detener movimiento
-            miDron.Mover("Stop", 0);
+            try
+            {
+                if (recognizer != null)
+                {
+                    recognizer.RecognizeAsyncCancel();
+                    recognizer.RecognizeAsyncStop();
+                    recognizer.Dispose();
+                    recognizer = null;
+                }
+            }
+            catch { }
         }
     }
 }
